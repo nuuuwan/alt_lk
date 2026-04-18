e361a368c75ca145b9fabefa606416af8d883cac
nuuuwan <nuuuwan@gmail.com>
2026-04-18 08:55:06 +0530

    cleanup unused files

--- src/alt_lk/render/__init__.py
+++
@@ -1,6 +0,0 @@
-# alt_lk.render (auto generate by build_inits.py)
-# flake8: noqa: F408
-
-from alt_lk.render.AbstractPlot import AbstractPlot
-from alt_lk.render.Map2D import Map2D
-from alt_lk.render.Map3D import Map3D
